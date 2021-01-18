2954ddd43fa73b709173a0d4c84195019a500950
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-01-18 14:47:49 +0000

    Update log_import.py

--- lib/modules/log_import.py
+++ lib/modules/log_import.py
@@ -15,15 +15,19 @@ def log_import():
             continue
         else:
             continue
-    
-    cmd2 = 'sudo cp /var/log/nginx/*.log ' + log_path
-    splited_cmd2 = cmd2.split()
-    run(splited_cmd2)
+    try:
+        cmd2 = 'sudo cp /var/log/nginx/*.log ' + log_path
+        #splited_cmd2 = cmd2.split()
+        #run(splited_cmd2)
 
-    cmd3 = 'sudo chmod 777 ' + log_path + '*.log' + 'biggie'
-    splited_cmd3 = cmd3.split()
-    run(splited_cmd3)
-    print('Nginx logs have been updated!')
+        cmd3 = 'sudo chmod 777 ' + log_path + '*.log' + ' biggie'
+        #splited_cmd3 = cmd3.split()
+        #run(splited_cmd3)
+        print('cmd2: ' + cmd2)
+        print('cmd3: ' + cmd3)
+        print('Nginx logs have been updated!')
+    except:
+        print('could not import')
 
     
 
