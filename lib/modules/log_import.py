b8ca3ea0b9c997a374d1ef29844cf616e2ef6131
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-01-18 14:45:18 +0000

    Update log_import.py

--- lib/modules/log_import.py
+++ lib/modules/log_import.py
@@ -1,18 +1,29 @@
 from lib.settings import log_path
 from subprocess import run
+import os, re
 
 def log_import():
 
-    cmd1 = 'rm ' + log_path + '*.log'
-    splited_cmd1 = cmd1.split()
-    run(splited_cmd1)
+    directory = os.fsencode(log_path)
 
-    cmd2 = 'sudo cp -r /var/log/nginx/*.log ' + log_path
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".log"):
+            cmd1 = 'rm ' + log_path + filename
+            splited_cmd1 = cmd1.split()
+            run(splited_cmd1)
+            continue
+        else:
+            continue
+    
+    cmd2 = 'sudo cp /var/log/nginx/*.log ' + log_path
     splited_cmd2 = cmd2.split()
     run(splited_cmd2)
 
-    cmd3 = 'sudo chmod 777 ' + log_path + '*.log'
+    cmd3 = 'sudo chmod 777 ' + log_path + '*.log' + 'biggie'
     splited_cmd3 = cmd3.split()
     run(splited_cmd3)
     print('Nginx logs have been updated!')
+
+    
 
