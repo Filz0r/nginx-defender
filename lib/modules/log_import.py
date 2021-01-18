c0287a399e07167852ece3ab9a60df3d77e1a07a
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-01-18 15:15:23 +0000

    Update log_import.py

--- lib/modules/log_import.py
+++ lib/modules/log_import.py
@@ -7,14 +7,15 @@ def log_import():
     directory = os.fsencode(log_path)
     cmd1 = 'sudo cp -a /var/log/nginx/. ' + log_path
     splited_cmd1 = cmd1.split()
-    Popen(splited_cmd1)
+    run(splited_cmd1)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
+        print('debug')
         if filename.endswith(".log"):
             cmd3 = 'sudo chown biggie ' + log_path + filename
-            splited_cmd3 = cmd3.split()
             print(cmd3)
-            Popen(splited_cmd3)
+            splited_cmd3 = cmd3.split()
+            run(splited_cmd3)
             continue
         else:
             print('did not run')
